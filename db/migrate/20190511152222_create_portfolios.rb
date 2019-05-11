a85cdc5404ce211e5bc1aea53d34eeba68c6b3cc
Kevin <wongkokwah@gmail.com>
2019-05-11 23:33:50 +0800

    created seed files for sample data

---
+++ db/migrate/20190511152222_create_portfolios.rb
@@ -0,0 +1,13 @@
+class CreatePortfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.string :main_image
+      t.string :thumb_image
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |x|
+    Blog.create!(
+        title: "My Blog Post #{x}",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras laoreet eu sapien eget venenatis. Quisque rutrum, lorem et dictum ornare, justo purus scelerisque tellus, a cursus orci ligula in neque. Quisque mattis augue tellus, et sagittis sapien facilisis aliquam. Fusce nec maximus quam. Curabitur tincidunt efficitur lectus, vitae tincidunt elit condimentum et. Donec dapibus consectetur ante eget accumsan. Etiam pretium sodales erat. Pellentesque in eleifend nisl. Nunc eget consectetur ante. Nam at nunc molestie, iaculis justo vel, ultrices quam. Quisque accumsan tempor lobortis. Praesent fermentum neque eget sapien auctor, id scelerisque dui facilisis. "
+    )
+end
+
+puts "10 blog post created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |item|
+    Portfolio.create!(
+        title: "Portfolio title #{item}",
+        subtitle: "My showcase",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultricies feugiat enim in iaculis. Aliquam imperdiet sollicitudin condimentum. Aenean risus dolor, cursus sed consequat nec, egestas vitae velit. In ac lorem et turpis blandit semper et a enim. Phasellus ullamcorper porta leo a aliquam. Proin eget dictum purus. Curabitur finibus, elit eu tincidunt ultricies, nunc dui rutrum augue, et suscipit. ",
+        main_image: "http://placehold.it/600x400",
+        thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+puts "9 portfolio created"
