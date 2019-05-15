9df6699a3a1896a097c08d6902bcc01bc7dd1e46
kevinW <wongkokwah@gmail.com>
2019-05-15 12:45:51 +0800

    Integrated New and Create actions for Portfolios

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
 
-  get 'portfolios/index'
-  
   resources :blogs
+  resources :portfolios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
