278225ba17503f1049e3802f57abb03831013281
kevin <kevin@betacode.io>
2019-10-28 09:30:43 +0800

    generate seed file

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_05_11_140122) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
+    t.string "subtitle"
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
